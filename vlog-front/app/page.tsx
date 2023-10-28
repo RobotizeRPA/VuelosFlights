88dac0898d9b455de6f35f6c06975443f566bd8e
PowChorba <101152834+PowChorba@users.noreply.github.com>
2023-10-28 11:03:31 -0300

    added revalidate

--- vlog-front/app/page.tsx
+++ vlog-front/app/page.tsx
@@ -2,10 +2,11 @@ import Home from "@/components/Home/Home";
 import Navbar from "@/components/Navbar/Navbar";
 import { getArticles } from "./service/home.service";
 
+export const revalidate = 200;
+
 export default async function App() {
   const dataArticles = await getArticles()
   const mainArticle = dataArticles.slice(0,5)
-
 
   return (
     <>
